f7a232978850359e4374f563cef1d54a3077d78f
evvieeeeee <evelinashchennikova@gmail.com>
2023-04-02 22:27:10 +0300

    update: added custom name to the project

---
+++ dzz6.py
@@ -0,0 +1,25 @@
+result = []
+def divider(a, b):
+ if a < b:
+    raise ValueError
+ if b > 100:
+    raise IndexError
+ if b == 0:
+    raise ZeroDivisionError
+ if a == []:
+    raise ArithmeticError
+ if a == "":
+    raise TypeError
+ return a/b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
+
+
+for key in data:
+   try:
+       res = divider(key, data[key])
+       result.append(res)
+   except Exception as err:
+       print(type(err))
+
+print(result)
