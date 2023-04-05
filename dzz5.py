a3c8f41e2c0667c3e21009a6dd2e9159342e72d5
evvieeeeee <evelinashchennikova@gmail.com>
2023-04-05 20:44:30 +0300

    update: added custom name to the project

---
+++ dzz5.py
@@ -0,0 +1,6 @@
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'Hello, Artur Andreevich')
+print(Fore.BLACK, Back.BLUE +'— How are you?')
+print(Style.DIM + 'I am okay')
+print(Style.RESET_ALL)
+print('go back')
